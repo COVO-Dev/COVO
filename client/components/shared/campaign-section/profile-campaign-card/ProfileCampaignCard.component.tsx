9df46475d557fd1fb2bd4a6cbf5a2bc46144c464
7medo-77 <ahmedabdelhamid1997@gmail.com>
2025-07-27 11:15:54 +0300

    Refactor ProfileCampaignCard component for improved readability and structure, including better handling of campaign data display

--- client/components/shared/campaign-section/profile-campaign-card/ProfileCampaignCard.component.tsx
+++ client/components/shared/campaign-section/profile-campaign-card/ProfileCampaignCard.component.tsx
@@ -5,71 +5,91 @@ import { format } from "date-fns";
 import { enUS } from "date-fns/locale";
 import IProfileCampaignCardProps from "./ProfileCampaignCard.model";
 
-export default function ProfileCampaignCard({ campaignData, handleCardClick }: IProfileCampaignCardProps) {
+export default function ProfileCampaignCard({
+  campaignData,
+  handleCardClick,
+}: IProfileCampaignCardProps) {
   return (
     <Card
       className="w-full rounded-md border col-span-2 md-lg:col-span-1 hover:bg-gray-200 duration-300 hover:scale-105"
-      onClick={() => handleCardClick(campaignData.brandId._id, campaignData._id)}
+      onClick={() =>
+        handleCardClick(campaignData.brandId._id, campaignData._id)
+      }
     >
       <CardHeader>
         <CardTitle className="flex text-lg font-semibold justify-between items-center">
           <span className="truncate max-w-[80%]">{campaignData.title}</span>
-          <span className={`text-base ${campaignData.status.toLowerCase() === 'active' ? 'text-green-500'
-            : campaignData.status.toLowerCase() === 'pending' ?
-              'text-yellow-500' : campaignData.status.toLowerCase() === 'completed' ?
-                'text-blue-500' :
-                'text-gray-500'
-            }  `}>{campaignData.status.toUpperCase()}</span>
+          <span
+            className={`text-base ${
+              campaignData.status.toLowerCase() === "active"
+                ? "text-green-500"
+                : campaignData.status.toLowerCase() === "pending"
+                ? "text-yellow-500"
+                : campaignData.status.toLowerCase() === "completed"
+                ? "text-blue-500"
+                : "text-gray-500"
+            }  `}
+          >
+            {campaignData.status.toUpperCase()}
+          </span>
         </CardTitle>
         <p className="text-sm text-muted-foreground">
-          Brand: {campaignData.brandId.firstName} {campaignData.brandId.lastName}
+          Brand: {campaignData.brandId.firstName}{" "}
+          {campaignData.brandId.lastName}
         </p>
       </CardHeader>
       <CardContent className="grid gap-4 grid-cols-2">
-
         <div className="col-span-1">
           <div className="flex items-center space-x-2">
             <Target className="h-4 w-4 min-w-[1em] text-muted-foreground" />
-            <span className="text-sm truncate">Target Audience: {campaignData.targetAudience}</span>
+            <span className="text-sm truncate">
+              Target Audience: {campaignData.targetAudience.ageGroups}
+            </span>
           </div>
           <div className="flex items-center space-x-2">
             <Coins className="h-4 w-4 min-w-[1em] text-muted-foreground" />
-            <span className="text-sm">Budget: ${campaignData.budgetRange}</span>
+            <span className="text-sm">Budget: {campaignData.budgetRange}</span>
           </div>
           <div className="flex items-center space-x-2">
             <MapPin className="h-4 w-4 min-w-[1em] text-muted-foreground" />
-            <span className="text-sm">Focus: {campaignData.geographicFocus}</span>
+            <span className="text-sm">
+              Focus: {campaignData.geographicFocus.country}
+            </span>
           </div>
           <div className="flex items-center space-x-2">
             <Users className="h-4 w-4 min-w-[1em] text-muted-foreground" />
-            <span className="text-sm">Influencer Type: {campaignData.influencerType}</span>
+            <span className="text-sm">
+              Influencer Type: {campaignData.influencerType}
+            </span>
           </div>
         </div>
 
         <div>
-          {campaignData.primaryGoals && campaignData.primaryGoals.length > 0 && (
+          {campaignData.primaryGoals && (
             <div>
               <div className="flex items-center space-x-2">
                 <Flag className="h-4 w-4 min-w-[1em] text-muted-foreground" />
-                <span className="text-sm">Goals:</span>
+                <span className="text-sm">Goal:</span>
               </div>
-              <ul className="list-disc pl-6 text-sm">
-                {campaignData.primaryGoals.map((goal) => (
-                  <li key={goal}>{goal}</li>
-                ))}
-              </ul>
+              <p className="pl-6 text-sm truncate">
+                {campaignData.primaryGoals}
+              </p>
             </div>
           )}
           <div className="flex items-center space-x-2">
             <Calendar className="h-4 w-4 min-w-[1em] text-muted-foreground" />
             <span className="text-sm">
-              Start Date: {format(new Date(campaignData.startDate), "PPP", { locale: enUS })}
+              Start Date:{" "}
+              {format(new Date(campaignData.startDate), "PPP", {
+                locale: enUS,
+              })}
             </span>
           </div>
           <div className="flex items-center space-x-2">
             <Calendar className="h-4 w-4 min-w-[1em] text-muted-foreground" />
             <span className="text-sm">
-              End Date: {format(new Date(campaignData.endDate), "PPP", { locale: enUS })}
+              End Date:{" "}
+              {format(new Date(campaignData.endDate), "PPP", { locale: enUS })}
             </span>
           </div>
           {/* <div>
@@ -79,6 +99,6 @@ export default function ProfileCampaignCard({ campaignData, handleCardClick }: I
       </CardContent>
     </Card>
   );
-};
+}
 
 // export default UserCampaignInfoCard;
